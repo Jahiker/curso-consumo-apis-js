7beab35fffd4aaba78617e6c327a9aac97187b7d
jahiker <rojasjahiker@gmail.com>
2022-11-23 00:22:39 -0400

    metodos HTTP

--- first_api/index.js
+++ first_api/index.js
@@ -1,8 +1,8 @@
-const URL = "https://api.thecatapi.com/v1/images/search?limit=3";
+const URL = "https://api.thecatapi.com/v1/images/search?limit=2";
 
 const btn = document.querySelector(".btn");
 
-const getKitten = async () => {
+const loadRandomMichis = async () => {
   const options = {
     method: "GET",
     headers: {
@@ -19,17 +19,41 @@ const getKitten = async () => {
 
       const img1 = document.querySelector("#img_1");
       const img2 = document.querySelector("#img_2");
-      const img3 = document.querySelector("#img_3");
 
       img1.src = data[0].url;
       img2.src = data[1].url;
-      img3.src = data[2].url;
     })
     .catch((err) => {
       throw new Error(err);
     });
 };
 
-getKitten();
+const loadFavoritesMichis = async () => {
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key":
+        "live_aCULxXarZdDfjky1JoyauJ8E7CfDnJrb7JcehmMcQ8ej1pfQJTMMWSl3aUHdxPDl",
+    },
+  };
+
+  await fetch(URL, options)
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data);
+
+      const img1 = document.querySelector("#img_1");
+      const img2 = document.querySelector("#img_2");
+
+      img1.src = data[0].url;
+      img2.src = data[1].url;
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+};
+
+loadRandomMichis();
 
 btn.addEventListener("click", getKitten.bind(this));
