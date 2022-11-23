33d17d098699e998845905896624d973aa239f84
jahiker <rojasjahiker@gmail.com>
2022-11-22 23:32:30 -0400

    [add] section favorites

--- first_api/index.js
+++ first_api/index.js
@@ -1,22 +1,21 @@
-
 const URL = "https://api.thecatapi.com/v1/images/search?limit=3";
 
 const btn = document.querySelector(".btn");
 
 const getKitten = async () => {
-
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
-      'x-api-key': 'live_aCULxXarZdDfjky1JoyauJ8E7CfDnJrb7JcehmMcQ8ej1pfQJTMMWSl3aUHdxPDl'
-    }
+      "Content-Type": "application/json",
+      "x-api-key":
+        "live_aCULxXarZdDfjky1JoyauJ8E7CfDnJrb7JcehmMcQ8ej1pfQJTMMWSl3aUHdxPDl",
+    },
   };
 
   await fetch(URL, options)
     .then((resp) => resp.json())
     .then((data) => {
-        console.log(data)
+      console.log(data);
 
       const img1 = document.querySelector("#img_1");
       const img2 = document.querySelector("#img_2");
@@ -25,7 +24,6 @@ const getKitten = async () => {
       img1.src = data[0].url;
       img2.src = data[1].url;
       img3.src = data[2].url;
-
     })
     .catch((err) => {
       throw new Error(err);
